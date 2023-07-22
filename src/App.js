0b745f32d062aa9b7f506b2434333844aa3a15c7
imjanny <wogns0808@gmail.com>
2023-07-22 21:21:16 +0900

    react component create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Counter(props){
+  console.log(props.title)
+  return <div>
+    <h1>{props.title}</h1>
+  <button>+</button> {props.initValue}
+  </div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Counter title="불면증 카운터" initValue={10} ></Counter> 
+        <Counter title="카운터2" initValue={5}></Counter>
+        <Counter title="counter" initValue={1}></Counter>
+      </div>
   );
 }
-
-export default App;
+// 중괄호 안에 있으면js의 데이터 타입으로 정수형이되고 , ""안에 있으면 문자열
+export default App; 
